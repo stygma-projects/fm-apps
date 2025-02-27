e7244b4022d80ee69505d73d25f60761900716df
Valentin FOURNY <fourny.valentin@gmail.com>
2025-02-27 20:54:34 +0100

    fix: add method

--- apps/api/src/router/productCategory.router.ts
+++ apps/api/src/router/productCategory.router.ts
@@ -1,8 +1,35 @@
 import { router, publicProcedure } from '../trpc';
+import prisma from "../prisma";
 
 export const productCategoryRouter = router({
     list: publicProcedure.query(() => {
-        // [..]
-        return [];
+       prisma.productCategory.findMany();
+    }),
+
+    create: publicProcedure.mutation((input: { name: string }) => {
+        prisma.productCategory.create({
+            data: {
+                name: input.name,
+            },
+        });
+    }),
+
+    update: publicProcedure.mutation((input: { id: number, name: string }) => {
+        prisma.productCategory.update({
+            where: {
+                id: input.id,
+            },
+            data: {
+                name: input.name,
+            },
+        });
+    }),
+
+    delete: publicProcedure.mutation((input: { id: number }) => {
+        prisma.productCategory.delete({
+            where: {
+                id: input.id,
+            },
+        });
     }),
 });
