2001ba3cd5b7c04bd26ab533a81d92e2ffaa590f
Valentin FOURNY <fourny.valentin@gmail.com>
2025-04-12 01:14:16 +0200

    fix: add test on postman

--- packages/trpc/src/index.ts
+++ packages/trpc/src/index.ts
@@ -1,9 +1,5 @@
-import {productCategoryRouter} from "./routes/inventory/productCategory.route";
-
 import { initTRPC} from '@trpc/server';
 import * as trpcExpress from "@trpc/server/adapters/express";
-import { ingredientCategoryRouter } from './routes/inventory/ingredientCategory.route';
-import { inventoryRouter } from './routes/inventory/inventory.route';
 
 export const createContext = ({ req, res }: trpcExpress.CreateExpressContextOptions) => ({})
 
@@ -14,9 +10,9 @@ const t = initTRPC.context<Context>().create();
 export const router = t.router;
 export const publicProcedure = t.procedure;
 
+import { inventoryRouter } from './routes/inventory/inventory.route';
+
 export const appRouter = router({
-    ingredientCategory: ingredientCategoryRouter,
-    productCategory: productCategoryRouter,
     inventory: inventoryRouter,
 });
 
