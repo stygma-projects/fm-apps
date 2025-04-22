d646968a9a62ecb9c6968525b901bc6d19fdbfda
Guilmin Leny <122307318+TarzanHR@users.noreply.github.com>
2025-04-22 14:24:14 +0200

    chore(db:studio): script db:studio (#32)

--- apps/fm-panel/src/pages/ProductCategories.page.vue
+++ apps/fm-panel/src/pages/ProductCategories.page.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="dark:text-white">{{ data }}</div>
+  <div class="dark:text-white">
+    <h1 class="">Catégories de Produits</h1><br/>
+    <div>
+      <div 
+        v-for="category in data" 
+        :key="category.id" 
+        class=""
+      >
+        <h2 class="">{{ category.label }}</h2>
+        <p v-if="category.imageUrl" class="">
+          Image disponible
+        </p>
+        <p v-else class="">
+          Pas d'image
+        </p>
+        <br>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useFetchProductCategories } from '../composables/productCategory.composable'
-
 const { data } = useFetchProductCategories()
 </script>
