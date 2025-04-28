35bb1085850a7701609bde0f3f0462922cfcd85d
Guilmin Leny <122307318+TarzanHR@users.noreply.github.com>
2025-04-28 10:04:35 +0200

    feat(trpc:ingredient): add ingredient CRUD routes (#51)
    * build(db:ingredient): split price into ht and ttc
    
    * feat(db:seed): add some data example for ingredient
    
    * feat(trpc:ingredient): add ingredient CRUD routes
    
    * chore(fm-apps:ci): update codeowners (#53)
    
    * style(toolkit:lint-staged): add lintstaged config
    
    * style(toolkit:lint-staged): add lintstaged config
    
    * style(toolkit:lint-staged): add lintstaged config
    
    * feat(db:seed): add some data example for ingredient
    
    * feat(trpc:ingredient): add ingredient CRUD routes
    
    * fix(fm-panel:fr): syntax correction
    
    ---------
    
    Co-authored-by: valentin-guiild <valentin@guiild.fr>
    Co-authored-by: Valentin Fourny <fourny.valentin@gmail.com>

--- packages/trpc/src/routes/inventory/inventory.route.ts
+++ packages/trpc/src/routes/inventory/inventory.route.ts
@@ -1,8 +1,10 @@
 import { productCategoryRouter } from './productCategory.route'
 import { ingredientCategoryRouter } from './ingredientCategory.route'
 import { router } from '../../trpc'
+import { ingredientRouter } from './ingredient.route'
 
 export const inventoryRouter = router({
   productCategory: productCategoryRouter,
   ingredientCategory: ingredientCategoryRouter,
+  ingredient: ingredientRouter,
 })
