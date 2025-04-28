c63e034793e0296e4e203e798da80c9f78e3ef0f
Yann <ciolkowskiyann@gmail.com>
2025-04-28 15:23:12 +0200

    feat(fm-panel:inventory.route.ts): added product route

--- packages/trpc/src/routes/inventory/inventory.route.ts
+++ packages/trpc/src/routes/inventory/inventory.route.ts
@@ -1,8 +1,10 @@
 import { productCategoryRouter } from './productCategory.route'
 import { ingredientCategoryRouter } from './ingredientCategory.route'
+import { productRouter } from './product.route'
 import { router } from '../../trpc'
 
 export const inventoryRouter = router({
   productCategory: productCategoryRouter,
   ingredientCategory: ingredientCategoryRouter,
+  product: productRouter,
 })
