a3784fb27462902545b4efb6b2632366b3e5fc29
TarzanHR <leny621000@gmail.com>
2025-05-06 09:45:48 +0200

    fix(fm-orders:pages): always show 6 columns with pending placeholders

--- apps/fm-orders/src/pages/DeliveryOrder.page.vue
+++ apps/fm-orders/src/pages/DeliveryOrder.page.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="grid grid-cols-6 w-full h-full">
+    <!-- Colonnes avec commandes -->
     <div
-      v-for="i in 6"
-      :key="i"
+      v-for="order in deliveryOrders"
+      :key="order.id"
       class="border-r border-gray-300 last:border-r-0 h-full"
     >
-      <CardOrder
-        v-if="deliveryOrders[i - 1]"
-        :order="deliveryOrders[i - 1]"
-        @refresh="handleRefresh"
-      />
-      <div v-else class="h-full flex items-center justify-center bg-gray-50">
+      <CardOrder :order="order" @refresh="handleRefresh" />
+    </div>
+    <!-- Colonnes vides si moins de 6 commandes -->
+    <div
+      v-for="i in 6 - deliveryOrders.length"
+      :key="'empty-' + i"
+      class="border-r border-gray-300 last:border-r-0 h-full"
+    >
+      <div class="h-full flex items-center justify-center bg-gray-50">
         <span class="text-gray-500">{{ t('pending') }}</span>
       </div>
     </div>
