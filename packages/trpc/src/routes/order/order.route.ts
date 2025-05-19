c233285b10348587a91baa9702db39a172c82e48
Yann <ciolkowskiyann@gmail.com>
2025-05-19 13:41:39 +0200

    fix(db:orders.prisma): added and implemented ProductInOrder table in database and pages

--- packages/trpc/src/routes/order/order.route.ts
+++ packages/trpc/src/routes/order/order.route.ts
@@ -1,6 +1,8 @@
 import { router } from '../../trpc'
 import { ordersRouter } from './orders.route'
+import { productInOrderRouter } from './productInOrder.route'
 
 export const orderRouter = router({
   orders: ordersRouter,
+  productInOrder : productInOrderRouter,
 })
