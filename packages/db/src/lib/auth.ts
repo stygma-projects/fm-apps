3883037b058e2b4d2075ef5278f68a13faf757a7
Yann <ciolkowskiyann@gmail.com>
2025-05-20 15:08:17 +0200

    feat(db:auth.ts): added prototype BetterAuth

---
+++ packages/db/src/lib/auth.ts
@@ -0,0 +1,10 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+import { prisma } from "../index";
+ 
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "mysql", // or "mysql", "postgresql", ...etc
+    }),
+});
