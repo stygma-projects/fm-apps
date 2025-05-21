a6f2efa7491a2b5f6637486ea2c4de025b5b8f9a
TarzanHR <leny621000@gmail.com>
2025-05-21 16:06:48 +0200

    feat(fm-customer:trpc): remove cache data in the server while in test

--- apps/fm-customer/src/composables/utils/useTrpcQuery.ts
+++ apps/fm-customer/src/composables/utils/useTrpcQuery.ts
@@ -1,11 +1,19 @@
 export const useTrpcQuery = <T>(
-    key: string,
-    queryFn: () => Promise<T>
-  ) => {
-    const { data, pending, error, refresh } = useAsyncData<T>(
-      key,
-      queryFn
-    )
-  
-    return { data, pending, error, refresh }
-  }
+  key: string,
+  queryFn: () => Promise<T>
+) => {
+  const isTesting = process.client && (window as any).Cypress
+
+  // Eviter que Nuxt se serve des mauvaises données depuis le cache
+  const cacheKey = isTesting ? `${key}-test` : key
+
+  const { data, pending, error, refresh } = useAsyncData<T>(
+    cacheKey,
+    queryFn,
+    {
+      server: !isTesting, // désactive le préchargement SSR en test
+    }
+  )
+
+  return { data, pending, error, refresh }
+}
