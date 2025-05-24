f4ddd6be400a54ec28b326d912c1bff3152cf35d
Yann <ciolkowskiyann@gmail.com>
2025-05-24 18:44:21 +0200

    fix(fm-customers:createAccount): backend account creation call WIP

--- apps/fm-customer/src/composables/api/user.composable.ts
+++ apps/fm-customer/src/composables/api/user.composable.ts
@@ -1,11 +1,17 @@
-import type { CreateUserOutput } from '@fm-apps/trpc'
+import type { signUpByMailAndPasswordInput, signUpByMailAndPasswordOutput } from '@fm-apps/trpc'
 import { useTrpcQuery } from '../utils/useTrpcQuery'
+import { useTrpcMutation } from '../utils/useTrpcMutation'
 
 export const useUser = () => {
   const { $trpc } = useNuxtApp()
-  
-  return useTrpcQuery<CreateUserOutput>(
-    'productCategory',
-    () => $trpc.inventory.productCategory.list.query()
+
+  const signUpByMailAndPassword = useTrpcMutation(
+    'user',
+    (input : signUpByMailAndPasswordInput) => $trpc.auth.user.signUpByMailAndPassword.mutate(input)
   )
+
+  return {    
+    signUpByMailAndPassword
+  }
+
 }
--- apps/fm-customer/src/pages/auth/create-account.vue
+++ apps/fm-customer/src/pages/auth/create-account.vue
@@ -3,12 +3,15 @@
         <Form @submit="onSubmit" class="p-5 flex flex-col border-solid border-1 border-gray-300 rounded-lg shadow-md" :validation-schema="schema" @invalid-submit="onInvalidSubmit">
             
             <h1 class="text-2xl mb-5">Création de compte</h1>
+
+            <h2 class="mb-2">Nom : </h2>
+            <Field class="border-solid border-1 border-gray-300 shadow-md mb-2" name="name" placeholder="Ex : Blank"/>
             
             <h2 class="mb-2">Email : </h2>
-            <Field class="border-solid border-1 border-gray-300 shadow-md mb-2" name="email" type="email" v-bind="emailAttrs" placeholder="aah"/>
+            <Field class="border-solid border-1 border-gray-300 shadow-md mb-2" name="email" type="email" placeholder="Ex : blank@gmail.com"/>
 
             <h2 class="mb-2">Mot de passe (au moins 8 caractères) : </h2>
-            <Field class="border-solid border-1 border-gray-300 shadow-md mb-2" name="password" type="password" v-model="password" v-bind="passwordAttrs" placeholder="aah"/>
+            <Field class="border-solid border-1 border-gray-300 shadow-md mb-2" name="password" type="password" placeholder=" Ex : D$1\\>0h1j4WO%_7oZ3e"/>
 
           <button class="mb-2 border-1 border-gray-300 rounded-lg shadow-md mx-auto w-auto pl-2 pr-2 pt-1 pb-1">Submit</button>
           <button class="mb-2 border-1 border-gray-300 rounded-lg shadow-md mx-auto w-auto pl-2 pr-2 pt-1 pb-1">Connect</button>
@@ -18,22 +21,29 @@
 
 <script setup>
 import { Form, Field } from 'vee-validate';
-// import { useToast } from '../../composables/utils/toast.composable';
+import { useUser } from '../../composables/api/user.composable';
 import * as yup from 'yup';
 
-const { successToast } = useToast();
+const { signUpByMailAndPassword } = useUser();
 
 const schema = yup.object({
+  name: yup.string().required(),
   email: yup.string().required().email(),
   password: yup.string().required().min(8),
 });
 
-function onSubmit(values) {
-  alert(JSON.stringify(values, null, 2));
+async function onSubmit(values) {
+  try {
+    const response = await signUpByMailAndPassword.mutate(values)
+    console.log('Réponse mutation:', response)
+    // Affiche un message de succès ou redirige
+  } catch (error) {
+    console.error('Erreur mutation:', error)
+    // Affiche un message d’erreur à l’utilisateur
+  }
 }
 
 function onInvalidSubmit({ values, errors, results }) {
-    successToast('danger', 'Invalid form submission');
     console.log('Invalid form submission', { values, errors, results });
 }
 </script>
