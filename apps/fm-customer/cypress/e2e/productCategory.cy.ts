da975cbdac697ecae5b9d2c2a3756be615e998f7
TarzanHR <leny621000@gmail.com>
2025-05-27 16:35:32 +0200

    feat(fm-customer:cart): cypress tests are done

--- apps/fm-customer/cypress/e2e/productCategory.cy.ts
+++ apps/fm-customer/cypress/e2e/productCategory.cy.ts
@@ -1,26 +1,32 @@
-import { getProductCategories } from './mocks/api/productCategory.api.cy';
+import { getProductCategories } from './mocks/api/productCategory.api.cy'
 
 describe('Product categories page', () => {
   beforeEach(() => {
     getProductCategories(200, 'productCategory.json')
-    cy.visit('/order/product-categories') 
-    cy.wait('@getProductCategories');
+    cy.visit('/order/product-categories')
+    cy.wait('@getProductCategories')
   })
 
   // Simulates a click on a category card to navigate to the products page.
   const navigateToCategory = () => {
-    cy.get('[data-cy="fmc-product-categories-card"]').contains('Boisson').click();
+    cy.get('[data-cy="fmc-product-categories-card"]')
+      .contains('Boissons')
+      .click()
     cy.url().should('match', /\/order\/.*\/products$/)
   }
 
   // Checks that the number of category cards on the page matches the number of product categories.
   const checkNumberOfCategoryInPage = () => {
     cy.get('@getProductCategories').then((interception: any) => {
-      const responseBody = interception.response?.body;
-      const res = responseBody.result.data.length;
-      cy.get('[data-cy="fmc-product-categories-card"]').should('have.length', res);
-    });
-  };
+      const responseBody = interception.response?.body
+      console.log(responseBody)
+      const res = responseBody.result.data.length
+      cy.get('[data-cy="fmc-product-categories-card"]').should(
+        'have.length',
+        res,
+      )
+    })
+  }
 
   // desktop
   context('Desktop', () => {
@@ -28,7 +34,10 @@ describe('Product categories page', () => {
       cy.viewport('macbook-11')
     })
 
-    it('navigating to the products page by clicking on a category card', navigateToCategory)
+    it(
+      'should navigate to the products page by clicking on a category card',
+      navigateToCategory,
+    )
 
     it('should verify that the number of category cards matches the number of categories from the API', () => {
       checkNumberOfCategoryInPage()
@@ -41,11 +50,13 @@ describe('Product categories page', () => {
       cy.viewport('iphone-x')
     })
 
-    it('navigating to the products page by clicking on a category card', navigateToCategory)
+    it(
+      'should navigate to the products page by clicking on a category card',
+      navigateToCategory,
+    )
 
     it('should verify that the number of category cards matches the number of categories from the API', () => {
       checkNumberOfCategoryInPage()
     })
   })
-
 })
