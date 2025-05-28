20db4fc0f25bf3a62b4529df75ce132b4ca21eb0
TarzanHR <leny621000@gmail.com>
2025-05-28 11:34:46 +0200

    feat(fm-customer:cart): use component for nuxtlink and use ingredient category mandatory

--- apps/fm-customer/src/components.d.ts
+++ apps/fm-customer/src/components.d.ts
@@ -8,6 +8,7 @@ export {}
 /* prettier-ignore */
 declare module 'vue' {
   export interface GlobalComponents {
+    'back-link.component': typeof import('./components/ui/back-link.component.vue')
     'card.component': typeof import('./components/ui/card.component.vue')['default']
     PrimeAccordion: typeof import('primevue/accordion')['default']
     PrimeAccordionTab: typeof import('primevue/accordiontab')['default']
