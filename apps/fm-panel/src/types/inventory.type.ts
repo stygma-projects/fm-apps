ae5b1c6f76bb69193c6339bed9f5f0c5f0eeb499
TarzanHR <leny621000@gmail.com>
2025-06-10 16:07:31 +0200

    refactor(fm-panel:page): display quantity of extra

--- apps/fm-panel/src/types/inventory.type.ts
+++ apps/fm-panel/src/types/inventory.type.ts
@@ -11,5 +11,43 @@ export type Product = NonNullable<GetByIdProductOutput>
 export type Ingredient = NonNullable<GetByIdIngredientOutput>
 export type IngredientCategory = NonNullable<GetByIdIngredientCategoryOutput>
 export type ProductCategory = NonNullable<GetByIdProductCategoryOutput>
-export type Order = NonNullable<GetByIdOrderOutput>
 export type User = NonNullable<GetByIdUserOutput>
+
+export interface ExtraIngredientInProductOrder {
+  id: string
+  productInOrderId: string
+  ingredientId: string
+  quantity: number
+  ingredient: {
+    id: string
+    label: string
+  }
+}
+
+export interface ProductInOrder {
+  id: string
+  productId: string
+  orderId: string
+  quantity?: number
+  product: {
+    id: string
+    label: string
+    nonUpdatable: {
+      id: string
+      label: string
+    }[]
+  }
+  mandatory: {
+    id: string
+    label: string
+  }[]
+  optionalBase: {
+    id: string
+    label: string
+  }[]
+  extraIngredients: ExtraIngredientInProductOrder[]
+}
+
+export interface Order extends Omit<NonNullable<GetByIdOrderOutput>, 'orderHasProduct'> {
+  orderHasProduct: ProductInOrder[]
+}
